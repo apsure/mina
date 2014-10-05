fd209ec61e4557cefbdc207a276b6158aabb4c7a
Nathan Palmer <nathan@nathanpalmer.com>
2014-10-05 07:58:38 -0400

    Updated bower task to invoke bower directly

--- lib/mina/bower.rb
+++ lib/mina/bower.rb
@@ -1,13 +1,15 @@
 namespace :bower do
-  set_default :bower_bin, 'bin/rake bower:'
-  set_default :bower_options, "RAILS_ENV=\"#{rails_env}\""
+  set_default :bower_bin, 'bower'
+  set_default :bower_prefix, "RAILS_ENV=\"#{rails_env}\""
+  set_default :bower_options, "-p"
+  set_default :bower_directory, ""
 
   # Installs assets.
   desc "Install dependencies using Bower."
   task :install do
     queue %{
       echo "-----> Installing dependencies using Bower"
-      #{echo_cmd %[#{bower_bin}install #{bower_options}]}
+      #{echo_cmd %[(cd #{bower_directory}; #{bower_prefix} #{bower_bin} install #{bower_options})]}
     }
   end
 end
