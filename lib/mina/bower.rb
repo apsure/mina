b5e804fbf3e3df7072beeb63db30085028a4577a
Nathan Palmer <nathan@nathanpalmer.com>
2014-10-05 07:59:21 -0400

    Reverted back to using bower-rails but chose the :deployment options of installation

--- lib/mina/bower.rb
+++ lib/mina/bower.rb
@@ -1,15 +1,13 @@
 namespace :bower do
-  set_default :bower_bin, 'bower'
-  set_default :bower_prefix, "RAILS_ENV=\"#{rails_env}\""
-  set_default :bower_options, "-p"
-  set_default :bower_directory, ""
+  set_default :bower_bin, 'bin/rake bower:'
+  set_default :bower_options, "RAILS_ENV=\"#{rails_env}\""
 
   # Installs assets.
   desc "Install dependencies using Bower."
   task :install do
     queue %{
       echo "-----> Installing dependencies using Bower"
-      #{echo_cmd %[(cd #{bower_directory}; #{bower_prefix} #{bower_bin} install #{bower_options})]}
+      #{echo_cmd %[#{bower_bin}install:deployment #{bower_options}]}
     }
   end
 end
